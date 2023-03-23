5b9007466f5ade2ced6fab7b9f7d1e6047a84555
phamvolinhnhi <phamvolinhnhi02@gmail.com>
2023-03-23 10:08:23 +0700

    Create solution

---
+++ Infrastructure/Persistence/AppicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces;
+using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+         : base(options)
+        {
+        }
+        public DbSet<AppSetting> AppSettings { get; set; }
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+    }
+}
